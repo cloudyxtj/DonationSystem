276f288ee2df0f7856efc2c330a542206cc9c757
chuahys <chuahys02@gmail.com>
2025-05-20 11:41:54 +0800

    separate feedback modal

--- inquiry/admin.py
+++ inquiry/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Inquiry
 
-@admin.register(Inquiry)
-class InquiryAdmin(admin.ModelAdmin):
-    list_display = ('inquiry_type', 'subject', 'email', 'created_at')
-    list_filter = ('inquiry_type', 'created_at')
-    search_fields = ('subject', 'email', 'message')
-    readonly_fields = ('created_at',)
-    ordering = ('-created_at',)
+# @admin.register(Inquiry)
+# class InquiryAdmin(admin.ModelAdmin):
+#     list_display = ('inquiry_type', 'subject', 'email', 'created_at')
+#     list_filter = ('inquiry_type', 'created_at')
+#     search_fields = ('subject', 'email', 'message')
+#     readonly_fields = ('created_at',)
+#     ordering = ('-created_at',)
+
+admin.site.register(Inquiry)
